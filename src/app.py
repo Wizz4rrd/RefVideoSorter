f934c3205e4c564ed35eec57d5376f2f35722ba0
Wizz4rrd <39056039+Wizz4rrd@users.noreply.github.com>
2023-11-28 19:38:03 +0100

    browse & show files

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.filedialog as fd
+import tkinter.ttk as ttk
+import os
 
 window = tk.Tk()
 window.title("Ref Video Converter")
@@ -7,7 +9,7 @@ window.title("Ref Video Converter")
 button = tk.Button(window, text="EXIT APP", width=25, command=window.destroy)
 button.pack()
 
-window.geometry("400x200")
+window.geometry("800x1200")
 window.eval('tk::PlaceWindow . center')
 window.attributes("-topmost",True)
 
@@ -18,12 +20,21 @@ lbl_path.pack()
 def get_directory():
     filepath = fd.askdirectory(initialdir="", title="Dialog box")
     dir_path.set(filepath)
+    list_files(filepath)
 
-
+def list_files(dir_path):
+    for item in tree_view.get_children():
+        tree_view.delete(item)
+    for filename in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, filename)):
+            tree_view.insert("","end",values=filename,)
+        
 
 dialog_btn = tk.Button(window, text="Select directory", command=get_directory)
 dialog_btn.pack()
 
-
+tree_view = ttk.Treeview(window, columns=('Files'), show="headings", selectmode="browse")
+tree_view.heading("Files",text="Files in directory")
+tree_view.pack(padx=20,pady=20, fill="both", expand=True)
 
 window.mainloop()
